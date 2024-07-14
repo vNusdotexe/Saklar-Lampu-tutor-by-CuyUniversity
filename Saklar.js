db80ca09e22d23178ea3964703c639444d044541
vNusdotexe <vrantojuliaokasari97@gmail.com>
2024-07-15 02:00:28 +0900

    Add files via upload

---
+++ Saklar.js
@@ -0,0 +1,28 @@
+function Saklar() {
+    let toggle1 = document.getElementById("default-toggle1")
+    let toggle2 = document.getElementById("default-toggle2")
+    let toggle3 = document.getElementById("default-toggle3")
+    let Lampu1 = document.getElementById('Lampu1')
+    let Lampu2 = document.getElementById('Lampu2')
+    let Lampu3 = document.getElementById('Lampu3')
+
+
+    toggle1.checked ? alert('On') : alert('Of');
+
+    // if (toggle1.checked) {
+    //     Lampu1.src = "Assets/Images/on.gif";
+    // } else {
+    // Lampu1.src = 'Assets/Images/off.gif';
+    // }
+    // if (toggle2.checked) {
+    //     Lampu2.src = "Assets/Images/on.gif";
+    // } else {
+    // Lampu2.src 
+    // = 'Assets/Images/off.gif';
+    // }
+    // if (toggle3.checked) {
+    //     Lampu3.src = "Assets/Images/on.gif";
+    // } else {
+    // Lampu3.src = 'Assets/Images/off.gif';
+    // }
+}
